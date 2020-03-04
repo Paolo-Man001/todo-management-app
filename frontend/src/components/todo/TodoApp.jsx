f58a85714603185085e1b3c420d77c411b046fd4
Paolo-Man001 <p.a.2manlunas@gmail.com>
2020-03-05 12:45:25 +1300

    Initialise 'todo' component-folder with TodoApp.jsx. Test to display Component.

---
+++ frontend/src/components/todo/TodoApp.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class TodoApp extends Component {
+   render() {
+      return (
+          <div className={"TodoApp"}>
+             <h1>From TodoApp Component!</h1>
+          </div>
+      );
+   }
+}
+
+export default TodoApp
