d5be69842c412bac2d7bb2698f158fea65b996c1
Paolo-Man001 <p.a.2manlunas@gmail.com>
2020-03-05 12:54:14 +1300

    Create a basic login form, only with 'User Name','Password' and 'Login Button'. Test and display.

--- frontend/src/components/todo/TodoApp.jsx
+++ frontend/src/components/todo/TodoApp.jsx
@@ -3,11 +3,28 @@ import React, { Component } from "react";
 class TodoApp extends Component {
    render() {
       return (
-          <div className={"TodoApp"}>
+          <div className={ "TodoApp" }>
              <h1>From TodoApp Component!</h1>
+             <LoginComponent/>
           </div>
       );
    }
 }
+
+
+//-------LoginComponent-------//
+
+class LoginComponent extends Component {
+   render() {
+      return (
+          <div>
+             User Name: <input type="text" name="username"/>
+             Password: <input type="text" name="password"/>
+             <button>Login</button>
+          </div>
+      );
+   }
+}
+
 
 export default TodoApp
