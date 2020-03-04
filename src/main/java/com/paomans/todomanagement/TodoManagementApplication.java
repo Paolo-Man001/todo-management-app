d9fd5f7b2a21b5a089f21c0f0adbc58fbc09053d
Paolo-Man001 <p.a.2manlunas@gmail.com>
2020-03-05 12:01:02 +1300

    Initial Commit. Initial setup Java Spring Boot backend.

---
+++ src/main/java/com/paomans/todomanagement/TodoManagementApplication.java
@@ -0,0 +1,13 @@
+package com.paomans.todomanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoManagementApplication.class, args);
+	}
+
+}
