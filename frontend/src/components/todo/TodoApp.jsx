5634b572d7c9980ef07d8f1a93a4ccc44c6ec230
Paolo-Man001 <p.a.2manlunas@gmail.com>
2020-03-05 22:56:16 +1300

    sec4-stp5   Add Authentication for 'username' and  'password'.   Use Hardcoded(temporary) data for auth-logic when 'Login' Button is Clicked (temp-auth-data in login-method).   Use Function-Component to return HTML-element to render Auth-Message to user.

--- frontend/src/components/todo/TodoApp.jsx
+++ frontend/src/components/todo/TodoApp.jsx
@@ -9,43 +9,78 @@ class TodoApp extends Component {
           </div>
       );
    }
-}
+}  // END Component-Parent: TodoApp
 
 
 //-------LoginComponent-------//
-
 class LoginComponent extends Component {
    constructor( props ) {
       super(props);
       this.state = {
          username: 'user name',
-         password: ''
+         password: '',
+         isLoginFail: false,
+         showSuccessMessage: false
       };
       // this.handlePasswordChange = this.handlePasswordChange.bind(this);
 
       this.handleChange = this.handleChange.bind(this);
-   }
+      this.loginClick = this.loginClick.bind(this);
+   }  // END Constructor
 
    render() {
       return (
           <div>
+             <ShowInvalidCredentials isLoginFail={ this.state.isLoginFail }/>
+             <ShowIValidCredentials showSuccessMessage={ this.state.showSuccessMessage }/>
              User Name: <input value={ this.state.username } type="text" name="username" onChange={ this.handleChange }/>
              Password: <input value={ this.state.password } type="password" name="password" onChange={ this.handleChange }/>
-             <button>Login</button>
+             <button onClick={ this.loginClick }>Login</button>
           </div>
       );
    }
 
+   //---- METHODS:
    handleChange( event ) {
-      console.log(this.state);
+      // console.log(this.state);
       this.setState({
-         // 'event.target.name' refers to the attribute 'name' that is assigned to "username / password"
-         // [event.target.name] MUST be in a bracket, to make it an object-member-variable(key)
+         /* 'event.target.name' refers to the attribute 'name' that is assigned to "username / password"
+          [event.target.name] MUST be in a bracket, to make it an object-member-variable(key)*/
          [event.target.name]: event.target.value
       })
-   }
+   } // END Method: handleChange()
 
+   loginClick() {
+      // DEFAULT HARDCODED AUTH = username: "john doe" , password: "password"
+      if ( this.state.username === "john doe" && this.state.password === "password" ) {
+         this.setState({ showSuccessMessage: true });
+         this.setState({ isLoginFail: false });
+         console.log("SUCCESS!");
+      } else {
+         this.setState({ showSuccessMessage: false });
+         this.setState({ isLoginFail: true });
+         console.log("FAIL");
+      }
+   } // END Method: loginClick()
+
+}// END Component-Child:  loginComponent
+
+
+//-------ShowInvalidCredentials-------//
+function ShowInvalidCredentials( props ) {
+   if ( props.isLoginFail ) {
+      return <h3>Invalid Credentials</h3>;
+   }
+   return <></>;
 }
 
+
+//-------ShowIValidCredentials-------//
+function ShowIValidCredentials( props ) {
+   if ( props.showSuccessMessage ) {
+      return <h3>Login Successful!!!</h3>;
+   }
+   return <></>;
+}
 
 export default TodoApp
