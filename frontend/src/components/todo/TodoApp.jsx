8c7e5564aa0c24661b03360cff7375a51232cdc7
Paolo-Man001 <p.a.2manlunas@gmail.com>
2020-03-05 21:01:20 +1300

    sec4-step3 make Controlled Component for form. Add onChange-attr in <input>. pass change-handlers(methods) in onChange-attr to handle 'event' changes to update state.

--- frontend/src/components/todo/TodoApp.jsx
+++ frontend/src/components/todo/TodoApp.jsx
@@ -15,14 +15,38 @@ class TodoApp extends Component {
 //-------LoginComponent-------//
 
 class LoginComponent extends Component {
+   constructor( props ) {
+      super(props);
+      this.state = {
+         username: 'user name',
+         password: ''
+      };
+      this.handleUsernameChange = this.handleUsernameChange.bind(this);
+      this.handlePasswordChange = this.handlePasswordChange.bind(this);
+   }
+
    render() {
       return (
           <div>
-             User Name: <input type="text" name="username"/>
-             Password: <input type="text" name="password"/>
+             User Name: <input value={ this.state.username } type="text" name="username" onChange={ this.handleUsernameChange }/>
+             Password: <input value={ this.state.password } type="password" name="password" onChange={ this.handlePasswordChange }/>
              <button>Login</button>
           </div>
       );
+   }
+
+   handleUsernameChange( event ) {
+      console.log(event.target.value);
+      this.setState({
+         username: event.target.value
+      })
+   }
+
+   handlePasswordChange(event) {
+      console.log(event.target.value);
+      this.setState({
+         password: event.target.value
+      });
    }
 }
 
