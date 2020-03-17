554059140af811332ce8ec9aad6cc1823e721274
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-03-17 22:34:29 +1300

    sec-6-49 : Intialise + Handle Form-submission :   Add 'InitialValues' and 'onSubmit' props in <Formik>. Use InitialValues of this.state,   onSubmit() gets inputValues and from Form and console.log to Test it's working.

--- frontend/src/components/todo/TodoComponent.jsx
+++ frontend/src/components/todo/TodoComponent.jsx
@@ -8,24 +8,38 @@ class TodoComponent extends Component {
       super(props);
       this.state = {
          id: 1,
-         description: 'Learn Forms UI with Formik',
+         description: '',
          targetDate: moment(new Date()).format('YYYY-MM-DD')
+      };
+      this.onSubmit = this.onSubmit.bind(this);
+   }
+
+   onSubmit( values ) {
+      if ( values.description === '' ) {
+         alert('No blank description allowed!');
+         return;
       }
+      console.log(values);
    }
 
    render() {
+      let { description, targetDate } = this.state;
+
       return (
           <div>
              <h1>Todo</h1>
-             {/*Todo Component for id - { this.props.match.params.id }*/}
+             {/*Todo Component for id - { this.props.match.params.id }*/ }
              <div className="container">
-                <Formik>
+                <Formik
+                    initialValues={ { description, targetDate } } // This is required for us to assign the input data
+                    onSubmit={ this.onSubmit }
+                >
                    {
-                      ( props ) => (
+                      () => (
                           <Form>
                              <fieldset className="form-group">
                                 <label htmlFor="description" className="float-left">Description</label>
-                                <Field className="form-control" type="text"  id="description" name="description"/>
+                                <Field className="form-control" type="text" id="description" name="description"/>
                              </fieldset>
                              <fieldset className="form-group">
                                 <label htmlFor="targetDate" className="float-left">Target Date</label>
